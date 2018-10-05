b9621c2c68dc5543faed5a92edda44624f15b51b
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-10-05 01:21:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,20 @@ class ApplicationController < Sinatra::Base
       erb :new
     end
 
+    post '/recipes' do
+     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+     redirect to "/recipes/#{@recipe.id}"
+    end
+
+    get '/recipes/:id' do
+      @recipe = Recipe.find_by_id(params[:id])
+      erb :show
+    end
+
+    get '/recipes/:id/edit' do
+      @recipe = Recipe.find_by_id(params[:id])
+      erb :edit
+    end
+
+
 end
